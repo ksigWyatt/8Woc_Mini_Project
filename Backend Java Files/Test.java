b73e28f9140d9c956c00d3da56917c1eac2d50a1
Pinto18 <pinto18.nd@gmail.com>
2017-06-16 09:55:14 -0400

    Modularized functions inside of Test.java

--- Backend Java Files/Test.java
+++ Backend Java Files/Test.java
@@ -1,23 +1,16 @@
-
-import java.awt.List;
 import java.io.File;
 import java.io.IOException;
-//import java.util.Arrays;
 import java.util.ArrayList;
-
+import java.util.Collections;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import com.google.gson.Gson;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import java.io.FileWriter;
 import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.*;
@@ -26,29 +19,12 @@ import com.fasterxml.jackson.databind.*;
 public class Test {
  public static void main(String[] args) throws ClassNotFoundException, 
 	JsonParseException, JsonMappingException , IOException {
-	 
-	 JSONParser parser = new JSONParser();
-	 ObjectMapper mapper = new ObjectMapper();
-	 HttpClient client = HttpClientBuilder.create().build();
-	 JSONObject obj = new JSONObject();
-	 JSONItem items = new JSONItem();
-	 IMG image = new IMG();
-	 
-	 HttpPost httppost = new HttpPost("http://172.19.144.219:12345/images");
-	 FileBody bin = new FileBody(new File("Hello.png"));
-	 HttpEntity reqEntity = MultipartEntityBuilder.create()
-		        .addPart("bin", bin)
-		        .build();
-	httppost.setEntity(reqEntity);
-	HttpResponse response = client.execute(httppost);
-	
-    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    
-    String jsonString = EntityUtils.toString(response.getEntity());
-    ArrayList<JSONItem> temp = mapper.readValue(jsonString, new TypeReference<ArrayList<JSONItem>>() {});
+    String jsonString = sendPostRequest();
+    ArrayList<JSONItem> items = mapJSONObjects(jsonString);
     int i = 0;
     
-    for (JSONItem j: temp){
+    Collections.sort(items);
+    for (JSONItem j: items){
     	System.out.println("Letter number " + i);
     	System.out.println("X Start "+ j.getX_start());
     	System.out.println("Y Start " + j.getY_start());
@@ -56,5 +32,29 @@ public class Test {
     	System.out.println("Y Dim " + j.getY_dim());    	
     	i++;
     }
+ }
+ 
+ 
+ //function for sending POST request to server
+ //will return string created from JSON objects returned from server
+ public static String sendPostRequest() throws ClientProtocolException, IOException{
+	 HttpClient client = HttpClientBuilder.create().build();
+	 HttpPost httppost = new HttpPost("http://172.19.144.219:12345/images");
+	 FileBody bin = new FileBody(new File("/users/nicholasdipinto1/Desktop/Hello.png"));
+	 HttpEntity reqEntity = MultipartEntityBuilder.create()
+		        .addPart("bin", bin)
+		        .build();
+	httppost.setEntity(reqEntity);
+	HttpResponse response = client.execute(httppost);	
+	return EntityUtils.toString(response.getEntity());
+ }
+ 
+ //function will map the string containing the information on the JSON objects
+ //to an array list of objects and return that array list
+ public static ArrayList<JSONItem> mapJSONObjects(String sourceString) throws JsonParseException, JsonMappingException, IOException{
+	 ObjectMapper mapper = new ObjectMapper();
+	 mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	 ArrayList<JSONItem> temp = mapper.readValue(sourceString, new TypeReference<ArrayList<JSONItem>>() {});
+	 return temp;
  }
 }
